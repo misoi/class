16c88f58e25235934fe7826a7705d516113a4db5
misoi <cytnhia.chepkemoi@gmail.com>
2016-04-05 16:01:45 +0300

    classes in java

---
+++ Animal/src/animal/Animal.java
@@ -0,0 +1,26 @@
+
+package animal;
+
+
+public class Animal {
+ void walking(){
+ System.out.println("IAM WALKING");
+ }
+   void sounding(){
+   System.out.println("IAM SOUNDING");
+   }
+   
+   
+    public static void main(String[] args) {
+       Animal dog= new Animal();
+       dog.walking();
+       dog.sounding();
+        Dog dog1= new Dog();
+            dog1.bark();
+        Dog dogs=new Dog();
+           dogs.hump();
+        
+    };
+    }
+    
+
---
+++ Animal/src/animal/Dog.java
@@ -0,0 +1,14 @@
+
+package animal;
+
+public class Dog {
+   void bark(){
+   System.out.println("WOOF WOOF WOOF");
+   }; 
+   void hump(){
+   System.out.println("YEH! YEH!YEH!");
+   };
+   
+   
+   
+}
